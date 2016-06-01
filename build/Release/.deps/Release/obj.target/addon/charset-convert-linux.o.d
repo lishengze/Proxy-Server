6a857bb62e44ad12035d70f49b326b826e19bc97
lishengze <lishengz.cool@hotmail.com>
2016-06-01 16:22:05 +0800

    serverbranch

--- build/Release/.deps/Release/obj.target/addon/charset-convert-linux.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/addon/charset-convert-linux.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/4.2.4/include/node -I/root/.node-gyp/4.2.4/src -I/root/.node-gyp/4.2.4/deps/uv/include -I/root/.node-gyp/4.2.4/deps/v8/include -I../node_modules/nan -I../.  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/addon/charset-convert-linux.o.d.raw   -c -o Release/obj.target/addon/charset-convert-linux.o ../charset-convert-linux.cpp
-Release/obj.target/addon/charset-convert-linux.o: \
- ../charset-convert-linux.cpp ../charset-convert-linux.h
-../charset-convert-linux.cpp:
-../charset-convert-linux.h:
