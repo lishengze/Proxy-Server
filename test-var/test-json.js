5014e246da74ff97e69dadf52f636ebfd797fd44
lishengze <lishengz.cool@hotmail.com>
2016-08-11 17:08:35 +0800

    完成配置文件读取

--- test-var/test-json.js
+++ test-var/test-json.js
@@ -1,4 +1,38 @@
-var fs = require("fs");
-var jsContent = require("../new file/system-config.json");
+function getConfigData() {
+    var fs = require("fs");
+    var jsContent = require("../new file/system-config-utf8.json");
+    // var jsContent = require("../new file/test-config.json");
 
-console.log (jsContent.SystemConfig.Topics.length)
+    // 访问方式originalAttrDataAttrDefine[i].$.propertyName.
+    var originalAttrDataAttrDefine = jsContent.SystemConfig.AttrDefine[0].Attr; 
+    var transAttrData = [];
+
+    for (var i= 0 ; i < originalAttrDataAttrDefine.length; ++i) {
+        transAttrData[originalAttrDataAttrDefine[i].$.name] = {};
+        transAttrData[originalAttrDataAttrDefine[i].$.name].name      = originalAttrDataAttrDefine[i].$.name;
+        transAttrData[originalAttrDataAttrDefine[i].$.name].valueType = originalAttrDataAttrDefine[i].$.valueType;
+        transAttrData[originalAttrDataAttrDefine[i].$.name].interval  = originalAttrDataAttrDefine[i].$.interval;
+        transAttrData[originalAttrDataAttrDefine[i].$.name].comment   = originalAttrDataAttrDefine[i].$.comment;
+    }
+
+    console.log ('AttrDefine property numb: ' + originalAttrDataAttrDefine.length);
+    console.log (originalAttrDataAttrDefine[1].$.name)
+    console.log(transAttrData[originalAttrDataAttrDefine[1].$.name])
+
+    var originalAttrDataPerformance = jsContent.SystemConfig.PerformanceAttrs[0].Attr; 
+    for (i = 0 ; i < originalAttrDataPerformance.length; ++i) {
+        transAttrData[originalAttrDataPerformance[i].$.name] = {};
+        transAttrData[originalAttrDataPerformance[i].$.name].name      = originalAttrDataPerformance[i].$.name;
+        transAttrData[originalAttrDataPerformance[i].$.name].valueType = originalAttrDataPerformance[i].$.valueType;
+        transAttrData[originalAttrDataPerformance[i].$.name].interval  = originalAttrDataPerformance[i].$.interval;
+        transAttrData[originalAttrDataPerformance[i].$.name].comment   = originalAttrDataPerformance[i].$.comment;
+    }
+
+    console.log ('Performance property numb: ' + originalAttrDataPerformance.length);
+    console.log (originalAttrDataPerformance[1].$.name)
+    console.log(transAttrData[originalAttrDataPerformance[1].$.name])
+
+    return transAttrData;
+}
+
+getConfigData();
